33c298eef1c9a594d7f04fd3180d46b9b2b1ec71
Ben Cordes <cordes.ben@gmail.com>
2014-11-23 17:41:41 -0500

    missed the new file on previous checkin

---
+++ app/assets/javascripts/spell_school.js
@@ -0,0 +1,19 @@
+function translate_spell_school(id) {
+  if (id === "0") {
+    return 'Abjuration';
+  } else if (id === "1") {
+    return 'Conjuration';
+  } else if (id === "2") {
+    return 'Divination';
+  } else if (id === "3") {
+    return 'Enchantment';
+  } else if (id === "4") {
+    return 'Evocation';
+  } else if (id === "5") {
+    return 'Illusion';
+  } else if (id === "6") {
+    return 'Necromancy';
+  } else if (id === "7") {
+    return 'Transmutation';
+  } 
+}
